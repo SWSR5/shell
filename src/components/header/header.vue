e51a46eb1ef2c5c8e6fa862801889affa1cb37f7
SWSR5 <1290511854@qq.com>
2019-09-12 17:47:43 +0800

    first day

---
+++ src/components/header/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main">header</div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped lang="stylus">
+  .main
+    width:100%;
+    height:100px;
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Goods from '../components/goods/goods.vue'
+import Ratings from '../components/ratings/ratings.vue'
+import Seller from '../components/seller/seller.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/goods',
+      name: 'goods',
+      component: Goods
+    },
+    {
+      path: '/ratings',
+      name: 'ratings',
+      component: Ratings
+    },
+    {
+      path: '/seller',
+      name: 'seller',
+      component: Seller
     }
   ]
 })
